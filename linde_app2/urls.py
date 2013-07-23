c396696461f7613a9415b1d7a123f6c313c13e6e
cerkiewny <mstarzycki@gmail.com>
2013-07-23 12:55:53 +0900

    deleted the manage users because the admin backend will be used for that

--- linde_app2/urls.py
+++ linde_app2/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 from linde_app2.views import home_page, stocktaking, stocksheet
-from manage_users.views import listUser
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -13,8 +12,6 @@ urlpatterns = patterns('',
     url(r'^stocktaking/(?P<stocktaking_number>\d+)$', stocktaking, name='stocktaking'),
     url(r'^stocksheet/(?P<stocksheet_number>\d+)$', stocksheet, name='stocksheet'),
 
-
-    url(r'^users/', include('manage_users.urls')),
     url(r'^manage_data/', include('manage_data.urls')),
     url(r'^stocktakings/', include('generate_stocktaking.urls')),
     url(r'^insert/', include('insert_stocksheet.urls')),
--- manage_users/views.py
+++
@@ -1,5 +0,0 @@
-# Create your views here.
-from manage_users.views_app.ListUserView import ListUserView
-
-listUser = ListUserView.as_view()
-
