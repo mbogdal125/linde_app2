7f650e4f52d3fac80788000e73550ddff2c642c3
Rafal Czaja <mojepiwo@gmail.com>
2013-07-26 20:00:19 +0900

    modified:   generate_stocktaking/forms.py 	modified:   generate_stocktaking/templates/generate_stocktaking/GenerateStocktaking.html 	static/css/datepicker.css 	static/js/bootstrap-datepicker.js 	static/less/

--- generate_stocktaking/forms.py
+++ generate_stocktaking/forms.py
@@ -1,8 +1,18 @@
 from django.forms import ModelForm
+from django.db import models
 from linde_app2.models import Stocktaking
+from datetime import datetime
+
+def custom_datefield_for_form(f):
+	field = f.formfield()
+	if isinstance(f, models.DateField):
+		field.widget.format ='%d/%m/%Y'
+		field.widget.attrs.update({'class':'datepicker', 'value':datetime.today().strftime('%d/%m/%Y'), 'data-date-format':'dd/mm/yyyy'})
+	return field
 
 class GenStocktakingForm(ModelForm):
-    class Meta:
-        model = Stocktaking
-        fields = ['type', 'date']
+	formfield_callback = custom_datefield_for_form
+	class Meta:
+		model = Stocktaking
+		fields = ['type', 'date']
     
