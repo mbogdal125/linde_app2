61ce50172d89bb4e09d16e7510599b5312cc2b9e
cerkiewny <mstarzycki@gmail.com>
2013-07-28 15:43:29 +0900

    few display fixes and security fix for AgreeData

--- agree_stocksheet/views_app/AgreeDataView.py
+++ agree_stocksheet/views_app/AgreeDataView.py
@@ -1,7 +1,8 @@
+from django.http import Http404
 from django.views.generic import TemplateView
 from django.forms.formsets import BaseFormSet
 from django.forms import ModelForm
-from linde_app2.models import StockSheet, StockItem, Stocktaking
+from linde_app2.models import StockSheet, StockItem, Stocktaking, StocktakingStatus
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import RequestContext, loader
@@ -20,19 +21,30 @@ class AgreeDataView(TemplateView):
         context = super(AgreeDataView, self).get_context_data(**kwargs)
         if "stocksheet_number" in kwargs:
             context['stocksheet_number'] = kwargs['stocksheet_number']
-        context['items'] =  StockItem.objects.filter(id_stock_sheet__stock_sheet_number = kwargs['stocksheet_number'])
-        context['notes'] =  StockSheet.objects.get(stock_sheet_number=kwargs['stocksheet_number']).notes
-        return context
+            context['items'] =  StockItem.objects.filter(id_stock_sheet__stock_sheet_number = kwargs['stocksheet_number'])
+            context['notes'] =  StockSheet.objects.get(stock_sheet_number=kwargs['stocksheet_number']).notes
+            stocksheet = StockSheet.objects.get(stock_sheet_number = kwargs['stocksheet_number'])
+            if stocksheet.status.id not in {2,3}:
+                raise Http404
+            return context
+        return redirect("home")
 
     def post(self, request, **kwargs):
         response = ""
         post = request._get_post()
         stocksheet = StockSheet.objects.get(stock_sheet_number=kwargs['stocksheet_number'])
         if "stocksheet_number" in kwargs:
+            if stocksheet.status.id not in {2,3}:
+                raise Http404
             items = StockItem.objects.filter(id_stock_sheet__stock_sheet_number = kwargs['stocksheet_number'])
             for i in items:
                 StockItem.objects.filter(id = i.id).update(amount_real_agreed = post[str(i.id)])
             StockSheet.objects.filter(stock_sheet_number=kwargs['stocksheet_number']).update(status=3)
             if "notes" in post:
                 StockSheet.objects.filter(stock_sheet_number=kwargs['stocksheet_number']).update(notes=post["notes"])
-        return redirect('agree-chose-stocksheet', stocksheet.id_stocktaking.stocktaking_number)
+            #if its last one we will change stocktaking status to inserted
+        if StockSheet.objects.filter(id_stocktaking__stocktaking_number=stocksheet.id_stocktaking.stocktaking_number, status=2):
+            return redirect('agree-chose-stocksheet', stocksheet.id_stocktaking.stocktaking_number)
+        stocksheet.id_stocktaking.status=StocktakingStatus.objects.get(id=3)
+        stocksheet.id_stocktaking.save()
+        return redirect('agree-chose-stocktaking')
